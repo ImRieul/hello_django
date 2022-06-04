3516540413b686c046a97705e39db7e95e5a796a
mojja-im <82865792+ImRieul@users.noreply.github.com>
2022-06-05 05:52:13 +0900

    2장 장고의 기본 요소 익히기! ~04 조회와 템플릿

--- mysite/pybo/admin.py
+++ mysite/pybo/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from pybo.models import Question
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    search_fields = ['subject']
+
+
 # Register your models here.
+admin.site.register(Question, QuestionAdmin)
